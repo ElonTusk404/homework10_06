217805b53453d9b400b34a5c506a936a551d3277
ElonTusk404 <igmaflappy@gmail.com>
2023-06-11 00:03:29 +0400

    Create Printing_Printing_Printing.py

---
+++ Printing_Printing_Printing.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+#Используется \n для печати с новой строки
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#Вместо f строк просто запятая и переменная
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+print("""
+There's something going on here.
+With the three double-quotes.
+Well be able to type as much as we like.
+Even 4 lines if we want, or 5,or 6.
+""")
