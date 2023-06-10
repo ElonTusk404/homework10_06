591727ccfdf7a2a4f6d25e4f8c2e062492708fd0
ElonTusk404 <igmaflappy@gmail.com>
2023-06-11 00:03:26 +0400

    Create More_Printing.py

---
+++ More_Printing.py
@@ -0,0 +1,28 @@
+#Просто печать
+print("Mary had a litle lamb.")
+#добавляет snow используя {}.format
+print("Its flece was white as {}.".format('snow'))
+#Простая печать
+print("And everywere that Mary went.")
+#создает строку из детяти точек ..........
+print("." * 10)
+
+
+
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#end в этом случае использовался чтобы вторая строка кода print напечаталась в той же строке с первым принтом
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
